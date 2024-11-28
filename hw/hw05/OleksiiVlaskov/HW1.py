bdb9d4d1d2e54e129848636a4321a7cb82b350d8
al3xjbfirst <138674237+al3xjbfirst@users.noreply.github.com>
2024-11-28 19:05:09 +0200

    Update HW1.py
    another way to do this task

--- hw/hw05/OleksiiVlaskov/HW1.py
+++ hw/hw05/OleksiiVlaskov/HW1.py
@@ -2,11 +2,13 @@ print('''Task1. Create a list that contains elements of integer type, then use
 the loop to change the type of these elements to a floating type.
 
 ''')
-list1=[1,2,3,4,5,6,7,8]
-list2=[]
-for a in list1:
-    list2.append(float(a))
+list1=[2,1,3,4,5,6,7,8]
+# list2=[]
+# for a in list1:
+#     list2.append(float(a))
 
-print(f"{list2=}, where objects {type(list2[0])=} ")
+# print(f"{list2=}, where objects {type(list2[0])=} ")
     
-    
+for a, b in enumerate(list1):
+    list1[a]=float(b)
+print(list1)
