657fb0bcb6f1b547eb0847989e5263e3b1314f4e
Yuliia Shpylka <iuliia.shpilka+2@gmail.com>
2024-12-01 23:21:32 +0200

    Shpylka HW06

---
+++ hw/hw06/YuliiaShpylka/Task02.py
@@ -0,0 +1,8 @@
+
+while True:
+    loggin_word = input("Please enter your login: ")
+    if loggin_word == "First":
+        print("Greetings!")
+        break
+    else:
+        print("Login is unknown")
