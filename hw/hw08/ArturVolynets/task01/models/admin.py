289c30b4754082340dff37f6e3b317df6e17ebb5
Artur <volunetsartur@gmail.com>
2024-12-25 22:11:54 +0200

    added hw8

---
+++ hw/hw08/ArturVolynets/task01/models/admin.py
@@ -0,0 +1,7 @@
+def create_admin():
+    pass
+
+def delete_admin():
+    pass
+
+__all__ = ["create_admin"]
---
+++ hw/hw08/ArturVolynets/task01/models/user.py
@@ -0,0 +1,8 @@
+def create_user():
+    pass
+
+def delete_user():
+    pass
+
+__all__ = ["create_user"]
+
